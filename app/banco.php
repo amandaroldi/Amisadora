106d89281f9274d0e73d305d7293e1d2fdd5ead6
amandaroldi <amandaroldi5@gmail.com>
2026-09-21 09:50:41 -0300

    banco.php

--- app/banco.php
+++ app/banco.php
@@ -5,16 +5,24 @@ function banco($server, $user, $password, $db, $consulta)
     $banco = new mysqli($server, $user, $password, $db);
 
     if ($banco->connect_error) {
-        echo "Falha de conexão: (" . $banco->connect_errno . ") - " . $banco->connect_error;
+        echo "Falha de conexão: (" .
+             $banco->connect_errno .
+             ") - " .
+             $banco->connect_error;
         exit();
     }
 
-    if (!$resultado = $banco->query($consulta)) {
-        echo "Falha na consulta: (" . $banco->errno . ") - " . $banco->error;
+    $banco->set_charset("utf8mb4");
+
+    $resultado = $banco->query($consulta);
+
+    if (!$resultado) {
+        echo "Falha na consulta: (" .
+             $banco->errno .
+             ") - " .
+             $banco->error;
         exit();
     }
-
-    $banco->close();
 
     return $resultado;
 }
